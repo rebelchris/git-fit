37817346af8b9e3f3513d43b7555c1de135c5480
Chris Bongers <chrisbongers@gmail.com>
2026-01-20 10:42:19 +0200

    Initial Commit

---
+++ GitFit/GitFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitFitApp.swift
+//  GitFit
+//
+//  Created by Chris Bongers on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct GitFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
